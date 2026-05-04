6017bd3af6018b1e92dba21e87a504663d44393d
Raymond Weng <156469578+rayjweng@users.noreply.github.com>
2026-05-03 23:09:37 -0400

    Update prepare_image_dataset.py

--- BME450_FINAL-main/scripts/prepare_image_dataset.py
+++ BME450_FINAL-main/scripts/prepare_image_dataset.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split 
 
 
 def parse_args() -> argparse.Namespace:
